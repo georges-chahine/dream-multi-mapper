f9f58b834dfb7e2476f763e5f4f7fc7a8e58fe6c
Georges Chahine <chahine.georges@gmail.com>
2021-03-09 02:34:00 +0100

    bug

--- src/IO/IO.cpp
+++ src/IO/IO.cpp
@@ -737,14 +737,14 @@ void IO::readBags(std::string sourceBags, std::string currentPath, std::vector<s
         if (walkingMode&&i>0){   //cap velocity to human walking velocity with some flexibility
             double elapsedT=gpsUTMContainer.vect[i][0]-gpsUTMContainer.vect[i-1][0];
             double maxDisp=elapsedT*humanSpeed;
-            if (    (gpsUTMContainer.vect[i][1]-gpsUTMContainer.vect[i-1][1])>maxDisp   ){gpsUTMContainer.vect[i][1]=gpsUTMContainer.vect[i-1][1]+maxDisp;}
-            if (    (gpsUTMContainer.vect[i][1]-gpsUTMContainer.vect[i-1][1])<-maxDisp   ){gpsUTMContainer.vect[i][1]=gpsUTMContainer.vect[i-1][1]-maxDisp;}
+       //     if (    (gpsUTMContainer.vect[i][1]-gpsUTMContainer.vect[i-1][1])>maxDisp   ){gpsUTMContainer.vect[i][1]=gpsUTMContainer.vect[i-1][1]+maxDisp;}
+        //    if (    (gpsUTMContainer.vect[i][1]-gpsUTMContainer.vect[i-1][1])<-maxDisp   ){gpsUTMContainer.vect[i][1]=gpsUTMContainer.vect[i-1][1]-maxDisp;}
 
-            if (    (gpsUTMContainer.vect[i][2]-gpsUTMContainer.vect[i-1][2])>maxDisp   ){gpsUTMContainer.vect[i][2]=gpsUTMContainer.vect[i-1][2]+maxDisp;}
-            if (    (gpsUTMContainer.vect[i][2]-gpsUTMContainer.vect[i-1][2])<-maxDisp   ){gpsUTMContainer.vect[i][2]=gpsUTMContainer.vect[i-1][2]-maxDisp;}
+          //  if (    (gpsUTMContainer.vect[i][2]-gpsUTMContainer.vect[i-1][2])>maxDisp   ){gpsUTMContainer.vect[i][2]=gpsUTMContainer.vect[i-1][2]+maxDisp;}
+           // if (    (gpsUTMContainer.vect[i][2]-gpsUTMContainer.vect[i-1][2])<-maxDisp   ){gpsUTMContainer.vect[i][2]=gpsUTMContainer.vect[i-1][2]-maxDisp;}
 
-            if (    (gpsUTMContainer.vect[i][3]-gpsUTMContainer.vect[i-1][3])>maxDisp/2   ){gpsUTMContainer.vect[i][3]=gpsUTMContainer.vect[i-1][3]+maxDisp/2;}
-            if (    (gpsUTMContainer.vect[i][3]-gpsUTMContainer.vect[i-1][3])<-maxDisp/2   ){gpsUTMContainer.vect[i][3]=gpsUTMContainer.vect[i-1][3]-maxDisp/2;}
+            //if (    (gpsUTMContainer.vect[i][3]-gpsUTMContainer.vect[i-1][3])>maxDisp/2   ){gpsUTMContainer.vect[i][3]=gpsUTMContainer.vect[i-1][3]+maxDisp/2;}
+            //if (    (gpsUTMContainer.vect[i][3]-gpsUTMContainer.vect[i-1][3])<-maxDisp/2   ){gpsUTMContainer.vect[i][3]=gpsUTMContainer.vect[i-1][3]-maxDisp/2;}
 
 
         }
